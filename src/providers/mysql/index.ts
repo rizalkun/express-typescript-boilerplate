1c51ffe240db42097047cc70626bc51869f53c90
Rizal Hidayat <rizalhidayat180499@gmail.com>
2022-05-04 06:37:44 +0700

    fix issues model

--- src/providers/mysql/index.ts
+++ src/providers/mysql/index.ts
@@ -2,12 +2,11 @@ import 'reflect-metadata'
 
 import { MYSQL_URI } from '../../utils/constants'
 import { DataSource } from 'typeorm';
+import path from 'path'
 
 export const dataSource = new DataSource({
     type: 'mysql',
     url: MYSQL_URI,
-    entities: [
-        'src/models/**/*.ts'
-    ],
+    entities: [path.join(__dirname, '../../**/*.model{.ts,.js}')],
     synchronize: true
 })
